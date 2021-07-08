32be47972735e12b509aa5b66e5baaeb2908e16f
zhulym <zhulym@mail.ru>
2021-07-08 18:54:26 +0300

    feat: implement function test 13/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const newStrArr = str.split('');
+  if ((str.length % 2) !== 0) return false;
+  // if (str[0] === str[str.length - 1]) return false;
+  newStrArr.forEach(el => (!bracketsConfig.flat().includes(el)) ? false : true)
+
+  return true;
 }
+
+// console.log(check('{()|}}', [['{', '}'], ['|', '|']]))
+// check('|()|', [['(', ')'], ['|', '|']]) // -> true
+// check('|(|)', [['(', ')'], ['|', '|']]) // -> false
+// check('[(])', [['(', ')'], ['[', ']']]) // -> false
