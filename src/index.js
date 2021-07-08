775b671302459f99ec316c792a7014969edab63b
zhulym <zhulym@mail.ru>
2021-07-08 20:58:35 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  const newStrArr = str.split('');
-  if ((str.length % 2) !== 0) return false;
-  // if (str[0] === str[str.length - 1]) return false;
-  newStrArr.forEach(el => (!bracketsConfig.flat().includes(el)) ? false : true)
-
-  return true;
+  // if ((str.length % 2) !== 0) return false;                                       // test 2/20
+  // newStrArr.forEach(el => (!bracketsConfig.flat().includes(el)) ? false : true);  // test 13/20
+  const newConfig = bracketsConfig.map(el => el.join(''));                           // test 20/20
+  debugger;
+  for (let i = 0; i < newConfig.length; i++) {
+    if (str.includes(newConfig[i])) {
+      str = str.replace(newConfig[i], '');
+      i = -1;
+      console.log(str);
+    }
+    console.log(str);
+  }
+  return !str;
 }
 
-// console.log(check('{()|}}', [['{', '}'], ['|', '|']]))
-// check('|()|', [['(', ')'], ['|', '|']]) // -> true
-// check('|(|)', [['(', ')'], ['|', '|']]) // -> false
-// check('[(])', [['(', ')'], ['[', ']']]) // -> false
+// console.log(check('|()|', [['(', ')'], ['|', '|']])) // -> true
+// console.log(check('|(|)', [['(', ')'], ['|', '|']])) // -> false
+// console.log(check('[(])', [['(', ')'], ['[', ']']])) // -> false
